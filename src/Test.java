808701b96366616a9b612295cc1fc2f69e650be7
kapilsantilal <kapilbamania96@gmail.com>
2024-01-27 15:14:20 +0000

    Commit5

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("My name is Kapil");
+        System.out.println("Kapil");
         System.out.println("My name is Kapil, How are you");
     }
 }
